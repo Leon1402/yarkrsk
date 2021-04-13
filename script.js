5921c4f4fabead2b72aba426340da722f9006826
Никита Каплюхин <nikita-140294@mail.ru>
2021-04-13 21:50:56 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+const swiper = new Swiper('.swiper-container', {
+    slidesPerView: 1,
+    spaceBetween: 5,
+    allowTouchMove: true,
+
+    breakpoints: {
+        480: {
+            slidesPerView: 2,
+            allowTouchMove: true,
+        },
+        768: {
+            slidesPerView: 3,
+            allowTouchMove: false,
+        }
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
