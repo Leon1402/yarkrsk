7adc21a2146d44e41b5cd1b704c7b73ddfeb455e
Никита Каплюхин <nikita-140294@mail.ru>
2021-04-14 22:44:20 +0700

    done

--- script.js
+++ script.js
@@ -2,9 +2,10 @@ const swiper = new Swiper('.swiper-container', {
     slidesPerView: 1,
     spaceBetween: 5,
     allowTouchMove: true,
+    loop: true,
     lazy: true,
     autoplay: {
-        delay: 3500,
+        delay: 3000,
         disableOnInteraction: false,
     },
     breakpoints: {
@@ -22,3 +23,44 @@ const swiper = new Swiper('.swiper-container', {
         prevEl: '.swiper-button-prev',
     },
 });
+
+const list = document.querySelector('.header__nav')
+
+document.querySelector('.burger').addEventListener('click', e => {
+    list.classList.add('header__nav_active');
+    e.cancelBubble = true
+})
+
+document.querySelector('.header__close').addEventListener('click', () => {
+    list.classList.remove('header__nav_active');
+})
+
+list.addEventListener('click', e => {
+    e._isListClicked = true
+});
+
+window.addEventListener('click', e => {
+    if (!e._isListClicked) {
+        list.classList.remove('header__nav_active');
+    }
+})
+
+const modal = document.querySelector('.modal');
+
+document.querySelector('.hero__button').addEventListener('click', () => {
+    modal.classList.add('modal_active');
+})
+
+document.querySelector('.modal__close').addEventListener('click', () => {
+    modal.classList.remove('modal_active');
+})
+
+modal.querySelector('.modal__content').addEventListener('click', (e) => {
+    e._isModalClick = true
+})
+
+modal.addEventListener('click', (e) => {
+    if(!e._isModalClick) {
+        modal.classList.remove('modal_active');
+    }
+})
